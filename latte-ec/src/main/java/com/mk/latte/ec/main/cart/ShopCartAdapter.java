83848b247308ad0afe02ca6024c6baf348018d88
Aatoken <3410760440@qq.com>
2017-11-11 00:13:11 +0800

    1.购物车勾选 商品事件初步完成 2.未完成 点击最后一个商品是小钩子 与 全选的 事件关联  即：勾上所以的商品 ，全选被点击

--- latte-ec/src/main/java/com/mk/latte/ec/main/cart/ShopCartAdapter.java
+++ latte-ec/src/main/java/com/mk/latte/ec/main/cart/ShopCartAdapter.java
@@ -1,10 +1,14 @@
 package com.mk.latte.ec.main.cart;
 
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.AppCompatTextView;
+import android.view.View;
 
 import com.bumptech.glide.Glide;
 import com.joanzapata.iconify.widget.IconTextView;
+import com.mk.latte.app.Latte;
 import com.mk.latte.ec.R;
 import com.mk.latte.ui.recycle.MultipleFields;
 import com.mk.latte.ui.recycle.MultipleItemEntity;
@@ -21,17 +25,28 @@ import java.util.List;
 
 public class ShopCartAdapter extends MultipleRecyclerAdapter {
 
+    /**
+     * 是否为全部选中 默认全选， 存入到 ShopCartItemFields.IS_SELECTED
+     */
+    private boolean mIsSelectedAll = true;
 
     protected ShopCartAdapter(List<MultipleItemEntity> data) {
         super(data);
+
+
         //添加 购物车 item 布局
         addItemType(ShopCartItemType.SHOP_CART_ITEM, R.layout.item_shop_cart);
 
     }
 
 
+    public void setIsSelectedAll(boolean isSelectedAll) {
+        this.mIsSelectedAll = isSelectedAll;
+    }
+
+
     @Override
-    protected void convert(MultipleViewHolder holder, MultipleItemEntity entity) {
+    protected void convert(MultipleViewHolder holder, final MultipleItemEntity entity) {
         super.convert(holder, entity);
         switch (holder.getItemViewType()) {
             case ShopCartItemType.SHOP_CART_ITEM:
@@ -53,7 +68,6 @@ public class ShopCartAdapter extends MultipleRecyclerAdapter {
                 final AppCompatTextView tvCount = holder.getView(R.id.tv_item_shop_cart_count);
                 final IconTextView iconIsSelected = holder.getView(R.id.icon_item_shop_cart);
 
-
                 //赋值
                 tvTitle.setText(title);
                 tvDesc.setText(desc);
@@ -64,12 +78,51 @@ public class ShopCartAdapter extends MultipleRecyclerAdapter {
                         .apply(GlideUtil.RECYCLER_OPTIONS)
                         .into(imgThumb);
 
+                //是否为全选 存入到 IS_SELECTED
+                entity.setField(ShopCartItemFields.IS_SELECTED, mIsSelectedAll);
+
+                //根据数据状态显示左侧小对勾
+                final boolean isSelected = entity.getField(ShopCartItemFields.IS_SELECTED);
+                showIconSelected(iconIsSelected, isSelected);
+
+
+                //左侧小对勾点击事件
+                iconIsSelected.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        final boolean currentSelected = entity.getField(ShopCartItemFields
+                                .IS_SELECTED);
+                        //刷新 状态
+                        showIconSelected(iconIsSelected, !currentSelected);
+                        //保存 item
+                        entity.setField(ShopCartItemFields.IS_SELECTED, !currentSelected);
+
+
+                    }
+                });
 
                 break;
             default:
                 break;
         }
     }
+
+    /**
+     * 选中的 小对勾显示
+     *
+     * @param iconView
+     * @param isSelected
+     */
+    public void showIconSelected(IconTextView iconView, boolean isSelected) {
+        if (isSelected) {
+            iconView.setTextColor(ContextCompat.getColor(Latte
+                    .getApplicationContext(), R.color.app_main));
+        } else {
+            iconView.setTextColor(Color.GRAY);
+        }
+    }
+
+
 
 
 }
--- latte-ec/src/main/java/com/mk/latte/ec/main/cart/ShopCartDelegate.java
+++ latte-ec/src/main/java/com/mk/latte/ec/main/cart/ShopCartDelegate.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import com.joanzapata.iconify.widget.IconTextView;
 import com.mk.latte.delegates.bottom.BottomItemDelegate;
 import com.mk.latte.ec.R;
 import com.mk.latte.ec.R2;
@@ -14,8 +15,10 @@ import com.mk.latte.net.callback.ISuccess;
 import com.mk.latte.ui.recycle.MultipleItemEntity;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * @author lenovo
@@ -26,8 +29,67 @@ public class ShopCartDelegate extends BottomItemDelegate implements ISuccess {
 
 
     private ShopCartAdapter mAdapter = null;
+    /**
+     * 购物车数量标记
+     */
+    private int mCurrentCount = 0;
+    private int mTotalCount = 0;
+
     @BindView(R2.id.rv_shop_cart)
     RecyclerView mRecyclerView = null;
+    @BindView(R2.id.icon_shop_cart_select_all)
+    IconTextView mIconSelectAll = null;
+
+    @OnClick(R2.id.icon_shop_cart_select_all)
+    void onClickSelectAll() {
+        //isSlectedAll 与 mAdapter中的 mIsSelectedAll 要一致
+        final boolean isSlectedAll = (boolean) mIconSelectAll.getTag();
+        //更新点击图标的颜色
+        mAdapter.showIconSelected(mIconSelectAll, !isSlectedAll);
+        //更新 adapter 的 item 被选中
+        mAdapter.setIsSelectedAll(!isSlectedAll);
+        //更新 recycleView的数据
+        mAdapter.notifyItemRangeChanged(0, mAdapter.getItemCount());
+        //保存到Tag 上
+        mIconSelectAll.setTag(!isSlectedAll);
+    }
+
+    @OnClick(R2.id.tv_top_shop_cart_remove_selected)
+    void onClickRemoveSelectedItem() {
+        //获取所有的数据
+        final List<MultipleItemEntity> data = mAdapter.getData();
+        //要删除的数据
+        final List<MultipleItemEntity> deleteEntities = new ArrayList<>();
+        for (MultipleItemEntity entity : data) {
+            final boolean isSelected = entity.getField(ShopCartItemFields.IS_SELECTED);
+            if (isSelected) {
+                deleteEntities.add(entity);
+            }
+        }
+        for (MultipleItemEntity entity : deleteEntities) {
+            int removePosition;
+            final int entityPosition = entity.getField(ShopCartItemFields.POSITION);
+            if (entityPosition > mCurrentCount - 1) {
+                removePosition = entityPosition - (mTotalCount - mCurrentCount);
+            } else {
+                removePosition = entityPosition;
+            }
+            if (removePosition <= mAdapter.getItemCount()) {
+                mAdapter.remove(removePosition);
+                mCurrentCount = mAdapter.getItemCount();
+                //更新数据
+                mAdapter.notifyItemRangeChanged(removePosition, mCurrentCount);
+            }
+        }
+    }
+
+
+    @OnClick(R2.id.tv_top_shop_cart_clear)
+    void onClickClear() {
+        mAdapter.getData().clear();
+        mAdapter.notifyDataSetChanged();
+    }
+
 
     @Override
     public Object setLayout() {
@@ -36,6 +98,7 @@ public class ShopCartDelegate extends BottomItemDelegate implements ISuccess {
 
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
+
 
     }
 
@@ -49,8 +112,6 @@ public class ShopCartDelegate extends BottomItemDelegate implements ISuccess {
                 .success(this)
                 .build()
                 .get();
-
-
     }
 
 
@@ -60,12 +121,17 @@ public class ShopCartDelegate extends BottomItemDelegate implements ISuccess {
         final ArrayList<MultipleItemEntity> data = new ShopCartDataConvert().setJsonData
                 (response).convert();
 
-        mAdapter=new ShopCartAdapter(data);
+        mAdapter = new ShopCartAdapter(data);
 
-        final LinearLayoutManager manager=new LinearLayoutManager(getContext());
+        final LinearLayoutManager manager = new LinearLayoutManager(getContext());
         mRecyclerView.setLayoutManager(manager);
         mRecyclerView.setAdapter(mAdapter);
 
+        //初始化 是否为全选
+        mIconSelectAll.setTag(true);
+        mAdapter.showIconSelected(mIconSelectAll, true);
+        mCurrentCount = mAdapter.getItemCount();
+        mTotalCount = mAdapter.getItemCount();
 
     }
 
