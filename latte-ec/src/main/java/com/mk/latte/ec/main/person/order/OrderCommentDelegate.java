2d760c13d821ef34eb82175c54e516f97dcf38d3
Aatoken <3410760440@qq.com>
2018-05-01 20:13:07 +0800

    评论区域的 图片显示自定义控件的完成

--- latte-ec/src/main/java/com/mk/latte/ec/main/person/order/OrderCommentDelegate.java
+++ latte-ec/src/main/java/com/mk/latte/ec/main/person/order/OrderCommentDelegate.java
@@ -1,5 +1,6 @@
 package com.mk.latte.ec.main.person.order;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,7 +9,11 @@ import android.view.View;
 import com.mk.latte.delegates.LatteDelegate;
 import com.mk.latte.ec.R;
 import com.mk.latte.ec.R2;
+import com.mk.latte.ui.widget.AutoPhotoLayout;
 import com.mk.latte.ui.widget.StarLayout;
+import com.mk.latte.util.callback.CallBackManager;
+import com.mk.latte.util.callback.CallBackType;
+import com.mk.latte.util.callback.IGlobalCallBack;
 import com.mk.latte.util.toast.ToastUtils;
 
 import butterknife.BindView;
@@ -23,6 +28,8 @@ public class OrderCommentDelegate extends LatteDelegate {
     @BindView(R2.id.custom_star_layout)
     StarLayout mStarLayout = null;
 
+    @BindView(R2.id.custom_auto_photo_layout)
+    AutoPhotoLayout mAutoPhotoLayout = null;
 
 
     @OnClick(R2.id.top_tv_comment_commit)
@@ -38,6 +45,13 @@ public class OrderCommentDelegate extends LatteDelegate {
 
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View rootView) {
-
+        mAutoPhotoLayout.setDelegate(this);
+        CallBackManager.getInstance()
+                .addCallBack(CallBackType.ON_CROP, new IGlobalCallBack<Uri>() {
+                    @Override
+                    public void executeCallBack(@Nullable Uri args) {
+                        mAutoPhotoLayout.onCropTarget(args);
+                    }
+                });
     }
 }
